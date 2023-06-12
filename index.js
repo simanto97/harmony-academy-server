30c5f79f51ccdefa065bfb561da2f4db41a38331
simanto97 <saimumsimanto97@gmail.com>
2023-06-12 22:25:37 +0600

    last update

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("harmonyDB").collection("users");
     const classesCollection = client.db("harmonyDB").collection("classes");
